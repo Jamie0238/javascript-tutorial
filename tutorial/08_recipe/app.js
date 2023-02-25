ef0f80f77538adde713d28f1305bb8cb927c3c88
jamie0238 <dmswlsla9745@gmail.com>
2023-02-25 10:03:21 +0900

    js

---
+++ tutorial/08_recipe/app.js
@@ -0,0 +1,13 @@
+// Application ID 3dccc8d0
+// Application Keys 61989a3ef004c93213a391098db17c36
+
+const form = document.querySelector('form')
+const input = form.querySelector('input')
+const foodList = document.querySelector('.food_list')
+
+const APP_ID = '3dccc8d0'
+const APP_KEY = '61989a3ef004c93213a391098db17c36'
+
+function init() {}
+
+init()
